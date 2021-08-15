c80e5889c599e0dd631a4add44b56e62ad9aac86
serii <seriiburduja@gmail.com>
2021-08-15 09:01:28 +0300

    useEffect

--- React/react-useEffect.js
+++ React/react-useEffect.js
@@ -9,3 +9,17 @@
 Когда много useEffect, можно именовать стрелочные функции, чтобы понять что он делает. 
 Можно повесить addEventListener в useEffect, и через return функцию removeEventListener.
 
+
+  useEffect(() => {
+    let isMounted = true;
+    async function fetchPost() {
+      const result = await axios.get(`${API_AXIOS_URL}/post/${id}`);
+      if (isMounted) {
+        setPost(result.data);
+        setLoading(false);
+      }
+    }
+    fetchPost();
+
+    return () => (isMounted = false);
+  }, [post, id]);
