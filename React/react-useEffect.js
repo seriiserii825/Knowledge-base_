c73eb6c4bc53bfeb6049970a4a82c63b29af426e
serii <seriiburduja@gmail.com>
2021-08-17 10:28:37 +0300

    useEffect

--- React/react-useEffect.js
+++ React/react-useEffect.js
@@ -1,25 +1,22 @@
-  useEffect(() => {
-    console.log("counter ", count);
+useEffect(() => {
+  console.log("counter ", count);
 
-    return () => {
-      console.log('good buy clicker')
-    }
-  }, [count]);
+  return () => {
+    console.log("good buy clicker");
+  };
+}, [count]);
 
-Когда много useEffect, можно именовать стрелочные функции, чтобы понять что он делает. 
-Можно повесить addEventListener в useEffect, и через return функцию removeEventListener.
+//Когда много useEffect, можно именовать стрелочные функции, чтобы понять что он делает.
+//Можно повесить addEventListener в useEffect, и через return функцию removeEventListener.
 
+useEffect(() => {
+  const ourRequest = axios.CancelToken.source();
+  async function fetchPost() {
+    const result = await axios.get(`${API_AXIOS_URL}/post/${id}`);
+    setPost(result.data);
+    setLoading(false);
+  }
+  fetchPost();
 
-  useEffect(() => {
-    let isMounted = true;
-    async function fetchPost() {
-      const result = await axios.get(`${API_AXIOS_URL}/post/${id}`);
-      if (isMounted) {
-        setPost(result.data);
-        setLoading(false);
-      }
-    }
-    fetchPost();
-
-    return () => (isMounted = false);
-  }, [post, id]);
+  return () => ourRequest.cancel();
+}, [post, id]);
