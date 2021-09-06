473a6942fba5ecbfe7b985ff14529c7c57b64bb1
serii <seriiburduja@gmail.com>
2021-09-06 13:20:05 +0300

    rank-math

---
+++ Wordrpess/wp-rank-math.php
@@ -0,0 +1,42 @@
+<?php
+
+function SinglePageMeta($data)
+{
+	global $title, $image, $site_title, $description, $url;
+	$title = isset($data->titolo) ? $data->titolo : '';
+	$site_title = isset($data->titolo) ? get_bloginfo('name') . ' | ' . $data->titolo : get_bloginfo('name');
+	$description = isset($data->disposizione_interna) ? $title . ' | ' . $data->disposizione_interna : '';
+	$url = (site_url() . '/ads/?id=' . strval($data->id));
+	$image = $data->foto_list[0]->nome;
+	//	var_dump( $image );
+	//exit();
+	add_filter('rank_math/frontend/title', function () {
+		global $title;
+		return $title;
+	});
+	add_filter('rank_math/frontend/description', function () {
+		global $description;
+		return $description;
+	});
+	add_filter('rank_math/frontend/canonical', function () {
+		global $url;
+		return $url;
+	});
+	add_filter('rank_math/opengraph/url', function () {
+		global $url;
+		return $url;
+	});
+	add_filter('wpseo_canonical', function () {
+		global $url;
+		return $url;
+	});
+	add_filter("rank_math/opengraph/facebook/image", function ($image) {
+		global $image;
+		return $image;
+	});
+	add_filter("rank_math/opengraph/facebook/og_title", function () {
+		global $title;
+		return $title;
+	});
+}
+
