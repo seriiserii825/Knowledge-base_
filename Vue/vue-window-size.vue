a271dce3f0a2e95693813f17bfeb57251d1142ab
serii <seriiburduja@gmail.com>
2021-09-23 16:32:21 +0300

    window size

--- Vue/vue-window-size.vue
+++ Vue/vue-window-size.vue
@@ -1,16 +1,20 @@
 <script>
  data: {
-      windowWidth: 0,
+      showToggleMenu: false,
+      windowWidth: 0
     },
 },
 created(){
+    if (process.browser) {
       this.windowWidth = window.innerWidth;
       window.addEventListener("resize", this.windowResize);
+    }
 },
 methods: {
-      windowResize() {
-        this.windowWidth = window.innerWidth;
-      }
+    windowResize() {
+      this.windowWidth = window.innerWidth;
+      this.showToggleMenu = this.windowWidth <= 992;
+    },
 },
 destroyed() {
         window.removeEventListener("resize", this.windowResize);
