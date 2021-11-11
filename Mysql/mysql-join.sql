93fb60e8583c99fd69b39cf999a6d37afa161e6f
serii <seriiburduja@gmail.com>
2021-11-11 20:10:08 +0200

    heidysql

--- Mysql/mysql-join.sql
+++ Mysql/mysql-join.sql
@@ -1,4 +1,9 @@
 #contry and cities by code
 SELECT city.Name, country.Name from city, country where country.Code = city.CountryCode;
+SELECT city.Name, country.Name FROM city JOIN country ON country.Code = city.CountryCode;
+
+SELECT Customers.FirstName, Products.ProductName, Orders.CreatedAt
+  FROM Customers, Products, Orders
+    WHERE Customers.Id = Orders.CustomerId AND Products.id = Orders.ProductId;
 
 
