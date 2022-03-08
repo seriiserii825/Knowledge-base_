fae3ebd31afde18e5cf1a123634d747acaa8abe9
serii <seriiburduja@gmail.com>
2022-03-08 16:19:47 +0200

    feat: cookie

--- Javascript/js-cookie.js
+++ Javascript/js-cookie.js
@@ -1,66 +1,43 @@
-    cookieStore.getAll().then(cookies => {
-        cookies.forEach(item => {
-            const name = item.name;
-            let scope = "";
-            let type = "";
-            let part = "";
-            let expires = "";
+import { bodyHidden, bodyVisible } from './bodyHidden';
 
-            const expiresYear = new Date(item.expires).getFullYear();
-            const currentYear = new Date().getFullYear();
-            const remainsYear = expiresYear - currentYear;
-            switch (name) {
-                case "wpglobus-language":
-                case "wpglobus-language-old":
-                    scope = "Registra in modo anonimo la scelta dell'utente in merito alla lingua di visualizzazione del sito";
-                    type = "HTTP Cookie - Tecnico";
-                    part = "Prima parte";
-                    expires =  remainsYear === 1 ? "1 anno" : remainsYear;
-                    switch (remainsYear) {
-                        case 1:
-                            expires = "1 anno";
-                            break;
-                        case 2:
-                            expires = "2 anni";
-                            break;
-                        default:
-                            expires = "1 anno";
-                            break;
-                    }
-                    break;
-                case "cookie_notice_accepted":
-                    scope = "Registra se l'utente ha o meno visualizzato il coookie banner in modo da non mostrarlo nuovamente ad ogni visualizzazione di pagina";
-                    type = "HTTP Cookie - Tecnico";
-                    part = "Prima parte";
-                    expires =  remainsYear === 1 ? "1 anno" : remainsYear;
-                    switch (remainsYear) {
-                        case 1:
-                            expires = "1 anno";
-                            break;
-                        case 2:
-                            expires = "2 anni";
-                            break;
-                        case remainsYear > 2:
-                            expires = "Persistente";
-                            break;
-                        default:
-                            expires = "Persistente";
-                            break;
-                    }
-                    break;
-                default:
-                    break;
-            }
-            let table = document.querySelector('#privacy-table tbody');
-            table.insertAdjacentHTML('afterbegin', `
-               <tr>
-                    <td>${name}</td>
-                    <td>${scope}</td>
-                    <td>${expires}</td>
-                    <td>${type}</td>
-                    <td>${part}</td>
-                </tr>
-            `);
-        })
-    })
-    // cookieStore.get('NAME_OF_THE_COOKIE').then(cookies => console.log(cookies))
+export default function bonus_popup() {
+	const popup = document.querySelector('.bonus-popup');
+	const bonus_popup = getCookie('bonus_popup');
+	const close_popup_btn = document.querySelector('.bonus-popup__close');
+    const bonus_btn = document.querySelector('.bonus-popup .btn');
+	if (!bonus_popup) {
+		showPopup();
+	}
+	close_popup_btn.addEventListener('click', function () {
+		closePopup();
+	});
+
+	bonus_btn.addEventListener('click', function () {
+        setCookie('bonus_popup');
+        closePopup();
+	});
+
+	function getCookie(name) {
+		var matches = document.cookie.match(
+			new RegExp(
+				'(?:^|; )' +
+					name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') +
+					'=([^;]*)'
+			)
+		);
+		return matches ? decodeURIComponent(matches[1]) : undefined;
+	}
+	function setCookie(name) {
+		let cookie_date = new Date();
+		cookie_date.setYear(cookie_date.getFullYear() + 1);
+		document.cookie = name + '=rewind;expires=' + cookie_date.toUTCString();
+	}
+	function showPopup() {
+		bodyHidden();
+		popup.classList.add('active');
+	}
+	function closePopup() {
+		bodyVisible();
+		popup.classList.remove('active');
+	}
+}
