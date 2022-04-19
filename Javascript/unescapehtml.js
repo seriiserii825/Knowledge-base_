a3cf09410af4b659dc287c83bfd0399b10369362
serii <seriiburduja@gmail.com>
2022-04-19 11:05:02 +0300

    feat: unescape

---
+++ Javascript/unescapehtml.js
@@ -0,0 +1,9 @@
+function unescapeHtml(unsafe) {
+  return unsafe
+    .replace(/&amp;/g, "&")
+    .replace(/&lt;/g, "<")
+    .replace(/&gt;/g, ">")
+    .replace(/&quot;/g, '"')
+    .replace(/&#8217;/g, "'")
+    .replace(/&#039;/g, "'");
+}
