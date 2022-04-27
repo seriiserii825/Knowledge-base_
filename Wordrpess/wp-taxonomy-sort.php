a4d3c8c0d65e24ab9f3480e12aab26859f39f951
serii <seriiburduja@gmail.com>
2022-04-27 17:21:52 +0300

    refactor: taxonomy sort

--- Wordrpess/wp-taxonomy-sort.php
+++ Wordrpess/wp-taxonomy-sort.php
@@ -1,16 +1,16 @@
-                <?php
-                $terms = get_terms([
-                    'taxonomy' => $current_term->taxonomy,
-                    'parent' => $current_term->term_id,
-                    'hide_empty' => false
-                ]); ?>
-
-                <?php foreach ($terms as $term) : ?>
-                    <?php $term->sort_number = +get_field('taxonomy', $term)['sort_number']; ?>
-                <?php endforeach; ?>
-
-                <?php
-                usort($terms, function ($a, $b) {
-                    return $a->sort_number <=> $b->sort_number;
-                });
-                ?>
+<?php
+$terms = get_terms([
+    'taxonomy' => 'categorie',
+]);
+$newterms = array();
+foreach ($terms as $term) {
+    $order = get_field('order', $term); //THIS MY CUSTOM FIELD VALUE
+    $newterms[$order] = (object)array(
+        'name' => $term->name,
+        'slug' => $term->slug,
+        'term_id' => $term->term_id,
+        /* 'image' => get_field('image', $term) */
+    );
+    ksort( $newterms, SORT_NUMERIC );
+}
+?>
