c6285e4bee4fa2726c82baa62b627ab625886304
serii <seriiburduja@gmail.com>
2022-11-02 17:19:28 +0200

    feat: scroll js

--- Javascript/js-scroll.js
+++ Javascript/js-scroll.js
@@ -9,3 +9,30 @@ export default function scrollToBlock() {
         block.scrollIntoView({behavior: 'smooth'});
     });
 }
+
+// In to view
+function isInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+
+    );
+}
+
+
+const box = document.querySelector('.box');
+const message = document.querySelector('#message');
+
+document.addEventListener('scroll', function () {
+    const messageText = isInViewport(box) ?
+        'The box is visible in the viewport' :
+        'The box is not visible in the viewport';
+
+    message.textContent = messageText;
+
+}, {
+    passive: true
+});
