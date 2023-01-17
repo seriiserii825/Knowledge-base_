57296e1d4ac1751627add1424053ed1467f01e66
serii <seriiburduja@gmail.com>
2023-01-17 16:50:37 +0200

    vue-transition

--- Vue/animation/v-transition.vue
+++ Vue/animation/v-transition.vue
@@ -1,61 +1,24 @@
-<style>
-v-enter-from - стартовое значение
-v-enter-active  - плавность
-v-enter-to - финальное значение
-v-leave-from
-v-leave-active
-v-leave-to
-</style>
-// Все 6 классов нужно, когда используется transition
-Если исользозвать animation, нужны всего лишь 2 класса
-key - атрибут, если нужно анимировать элементы, которые находятся раядом.
-
-Class:
-enter-class
-enter-active-class
-enter-to-class (в версии 2.1.8+)
-leave-class
-leave-active-class
-leave-to-class (в версии 2.1.8+)
-
-mode:
-in-out: Сначала появляется новый элемент, и только после этого исчезает старый.
-out-in: Сначала исчезает старый элемент, и только после этого появляется новый.
-
-Длительность :duration="{ enter: 500, leave: 800 }"
-
-Group
-appear
-transition tag="ul" name="list"
-.list-enter
-.list-leave
-.list-move {
-        transition: all .5s ease;
+<template>
+  <transition name="popup">
+    <VueAgentsPopup
+      :site-url="siteUrl"
+      v-if="is_popup_visible"
+      :item="agent"
+      @close-popup="closePopup"
+    />
+  </transition>
+</template>
+<style type="text/css" media="screen">
+/* // when popup start or end */
+.popup-enter-active,
+.popup-leave-active {
+  transition: all 0.5s;
 }
-
-
-
-<style>
-.fade-enter-active {
-  animation: fadeIn 1s ease;
-}
-.fade-leave-active {
-  animation: fadeIn 1s ease;
-}
-@keyframes fadeIn {
-  0% {
-    opacity: 0; 
-  }
-  100% {
-    opacity: 1;
-  }
-}
-@keyframes fadeOut {
-  0% {
-    opacity: 1; 
-  }
-  100% {
-    opacity: 0;
-  }
+/* // when popup work */
+.popup-enter,
+.popup-leave-to {
+  opacity: 0;
+  transform: scale(1.1);
 }
 </style>
+
