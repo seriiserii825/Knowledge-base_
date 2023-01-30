2692490f35e50ffee371f98595cd37e93335ea45
serii <seriiburduja@gmail.com>
2023-01-30 17:32:52 +0200

    date js

--- Javascript/js-date.js
+++ Javascript/js-date.js
@@ -1,50 +1,57 @@
-const now = new Date(); // Дату помещаем в переменную now.
-const now = new Date('2011-02-22'); // Передаем дату в виде строки
-const now = new Date(2011, 2, 22, 14); 
- Цифры состоящие из одного символа не должны содержать 0. 
- 2 - месяц превращается в 3, так как месяцы начинаются с 0
- 14 - часов превращаются в 11 - (-3) 
-const now = new Date(timestamp); // Колличесво миллисекунд
-now.getFullYear();
-now.getMonth();
-now.getDate(); - число
-now.getDay(); - номер дня недели, нумерация начинается с воскрсенья 0
-now.getHours(); - местное время
-now.getUTCHours(); - время по utc
-now.getTimezoneOffset(); - Разница по utc в минутах
-now.getTime(); - timestamp
-now.setDate(4);
-new Date.parse('2002-05-01'); // ====  new Date('2011-02-22');
-
-let start new Date();
-
-for (let i = 0; i < 100000; i++){
-  let some = i ** 3;
+export const useFormatDate = (date) => {
+    const sections = date.split(' ');
+    const date_arr = sections[0].split('/');
+    const months = {
+        '01': 'Gennaio',
+        '02': 'Febbraio',
+        '03': 'Marzo',
+        '04': 'Aprile',
+        '05': 'Maggio',
+        '06': 'Giugno',
+        '07': 'Luglio',
+        '08': 'Agosto',
+        '09': 'Settembre',
+        '10': 'Ottobre',
+        '11': 'Novembre',
+        '12': 'Dicembre',
+    };
+    const gsDayNames = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ];
+    const date_str = date_arr[0] + ' ' + months[date_arr[1]] + ' ' + date_arr[2];
+    const time = sections[1];
+    const date_number = date_arr[0];
+    const week_word = gsDayNames[new Date(date_str).getDay()];
+    const month_number = date_arr[1];
+    const month_word = months[date_arr[1]];
+    const year = date_arr[2];
+    return {
+        date_number,
+        week_word,
+        month_number,
+        month_word,
+        year,
+        time
+    }
 }
 
-let end = new Date();
+formatDate(date) {
+  console.log(date, "date");
+  let options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: false
+  };
 
-let difference = end - start;
-
-console(difference); //in milliseconds
-
-
-================
-
-    formatDate(date) {
-      console.log(date, "date");
-      let options = {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        hour12: false
-      };
-
-      return new Intl.DateTimeFormat("ro", options).format(new Date(date));
-    }
-
-# unique id
-new Date().valueOf()
+  return new Intl.DateTimeFormat("ro", options).format(new Date(date));
+}
