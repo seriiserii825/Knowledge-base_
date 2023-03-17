822d7c515798b555c36ffac01c86972dd0fc8917
serii <seriiburduja@gmail.com>
2023-03-17 08:25:23 +0200

    ts-enum

--- Typescript/enum.ts
+++ Typescript/enum.ts
@@ -1,68 +1,62 @@
-// числовой или строковый enum
-enum Direction {
-  Up = 1, // по-умолчанию 1, для строковых enum значения обязательны
-  Down,
-  Left,
-  Right
+function compute() {
+  return 4;
 }
 
-enum Speed {
-  low = '60km/h',
-  high = '220km/h'
+enum StatusCode {
+  SUCCESS = 1,
+  IN_PROCESS = 2,
+  FAILED = compute(),
 }
 
-// гетерогенный еnum
-enum Decision {
-  Yes = 1,
-  No = 'false'
+const res = {
+  message: "Payment success",
+  status: StatusCode.SUCCESS,
+};
+
+if (res.status === StatusCode.SUCCESS) {
+  console.log("some");
 }
 
-// рассчетный еnum может быть только числовым
-function calcEnum(){
-  return 2;
-}
-enum CalculatedEnum {
-  Yes = 1,
-  No = calcEnum()
+// constant enum
+const enum Roles {
+  ADMIN,
+  USER,
 }
 
-// enum в runtime можно использовать как объект
-function carMove(obj: {low: string, high: string}) {}
-carMove(Speed)
+const res2 = Roles.ADMIN;
 
-// обратный маппинг, получить строковое значения какого-то из enum
-enum Test {
-  A
-}
-let test = Test.A;
-let nameA = Test[test];
-
-// enum const
-// если enum не будет изменятся, что лучше использовать как константу, меньше рессурсов употребляет
-const enum ConstEnum{
-  A,
-  B
+//-==================== Practice
+async function getFaqs(req) {
+  const res = await fetch("/faqs", {
+    method: "POST",
+    body: JSON.stringify(req),
+  });
+  const data = await res.json();
+  return data;
 }
 
-let d = ConstEnum.A;
-
-// Never
-enum Dice {
-  One = 1,
-  Two
+// ==================== Result
+enum QuestionStatus {
+  published = "published",
+  draft = "draft",
+  deleted = "deleted",
 }
-
-function runDice(dice: Dice){
-  switch (dice) {
-    case Dice.One:
-      return 'один';
-      break;
-    case Dice.Two:
-      return 'один';
-      break;
-
-    //если в Dice добавится еще один элемент, то a:never будет равен ему и выскочит сообщение об ошибке.
-    default:
-      const a:never = dice;
-  }
+async function getFaqs2({
+  topicId: string,
+  status: QuestionStatus,
+}): Promise<
+  {
+    question: string;
+    answer: string;
+    tags: [];
+    likes: number;
+    status?: QuestionStatus;// ? - optional
+  }[]
+> {
+  const res = await fetch("/faqs", {
+    method: "POST",
+    body: JSON.stringify(req),
+  });
+  const data = await res.json();
+  return data;
 }
