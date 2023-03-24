db9dd9de664bf3ea02192c09b56cb746e992b73a
serii <seriiburduja@gmail.com>
2023-03-24 17:57:43 +0200

    date js

--- Javascript/js-date.js
+++ Javascript/js-date.js
@@ -29,9 +29,9 @@ export const useFormatDate = (date) => {
     const month_number = date_arr[1];
     const month_word = months[date_arr[1]];
     const year = date_arr[2];
-    const date_default = new Date(date);
+    const param_for_date = date_arr[2] + '-' + date_arr[1] + '-' + date_arr[0];
+    const date_default = new Date(param_for_date);
     const week_word = gsDayNames[date_default.getDay()];
-    debugger;
     return {
         date_number,
         week_word,
