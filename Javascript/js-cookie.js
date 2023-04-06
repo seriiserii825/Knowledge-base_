52c8ee42260921c3936504e5395869fd6f1c841b
serii <seriiburduja@gmail.com>
2023-04-06 10:10:50 +0300

    cookie

--- Javascript/js-cookie.js
+++ Javascript/js-cookie.js
@@ -1,43 +1,22 @@
-import { bodyHidden, bodyVisible } from './bodyHidden';
-
-export default function bonus_popup() {
-	const popup = document.querySelector('.bonus-popup');
-	const bonus_popup = getCookie('bonus_popup');
-	const close_popup_btn = document.querySelector('.bonus-popup__close');
-    const bonus_btn = document.querySelector('.bonus-popup .btn');
-	if (!bonus_popup) {
-		showPopup();
-	}
-	close_popup_btn.addEventListener('click', function () {
-		closePopup();
-	});
-
-	bonus_btn.addEventListener('click', function () {
-        setCookie('bonus_popup');
-        closePopup();
-	});
-
-	function getCookie(name) {
-		var matches = document.cookie.match(
-			new RegExp(
-				'(?:^|; )' +
-					name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') +
-					'=([^;]*)'
-			)
-		);
-		return matches ? decodeURIComponent(matches[1]) : undefined;
-	}
-	function setCookie(name) {
-		let cookie_date = new Date();
-		cookie_date.setYear(cookie_date.getFullYear() + 1);
-		document.cookie = name + '=rewind;expires=' + cookie_date.toUTCString();
-	}
-	function showPopup() {
-		bodyHidden();
-		popup.classList.add('active');
-	}
-	function closePopup() {
-		bodyVisible();
-		popup.classList.remove('active');
-	}
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
