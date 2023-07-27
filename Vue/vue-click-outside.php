a12c5f92eacafecfe64a0b35c3599e6cf2b522d4
serii <seriiburduja@gmail.com>
2023-07-27 09:18:34 +0300

    click-outside

--- Vue/vue-click-outside.php
+++ Vue/vue-click-outside.php
@@ -1,17 +1,17 @@
-<script>
-  Vue.directive('click-outside', {
-    bind: function(el, binding, vnode) {
-      el.clickOutsideEvent = function(event) {
-        // here I check that click was outside the el and his children
-        if (!(el == event.target || el.contains(event.target))) {
-          // and if it did, call method provided in attribute value
-          vnode.context[binding.expression](event);
-        }
-      };
-      document.body.addEventListener('click', el.clickOutsideEvent)
-    },
-    unbind: function(el) {
-      document.body.removeEventListener('click', el.clickOutsideEvent)
-    },
-  });
+<script setup>
+import {ref} from 'vue'
+
+const elementRef = ref(null)
+    
+window.addEventListener('click', (event) => {
+    if (!elementRef.value.contains(event.target)){
+        console.log('click outside element')
+    }
+})
+
 </script>
+
+
+<template>
+  <div ref="elementRef">your element</div>
+</template>
