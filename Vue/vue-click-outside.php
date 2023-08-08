6eceeaea503843d6ca8a8b14b30a8db5a65ca401
serii <seriiburduja@gmail.com>
2023-08-08 12:31:55 +0300

    click-outside

--- Vue/vue-click-outside.php
+++ Vue/vue-click-outside.php
@@ -1,17 +1,39 @@
 <script setup>
-import {ref} from 'vue'
+import { onBeforeUnmount, onMounted } from "vue";
 
-const elementRef = ref(null)
-    
-window.addEventListener('click', (event) => {
-    if (!elementRef.value.contains(event.target)){
-        console.log('click outside element')
-    }
-})
+export default function useDetectOutsideClick(component, callback) {
+    if (!component) return;
+    const listener = (event) => {
+        if (
+            event.target !== component.value &&
+            event.composedPath().includes(component.value)
+        ) {
+            return;
+        }
+        if (typeof callback === "function") {
+            callback();
+        }
+    };
+    onMounted(() => {
+        window.addEventListener("click", listener);
+    });
+    onBeforeUnmount(() => {
+        window.removeEventListener("click", listener);
+    });
 
+    return { listener };
+}
 </script>
 
+//Select component
+<script>
+import useDetectOutsideClick from "../../composables/useDetectOutsideClick";
 
+const selectRef = ref(null);
+useDetectOutsideClick(selectRef, () => {
+    is_visible_options.value = false;
+});
+</script>
 <template>
-  <div ref="elementRef">your element</div>
+<div class="select-component" ref="selectRef"> </div>
 </template>
