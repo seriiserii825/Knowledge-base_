934dd4c095cb62d5831cd5598b0ffaa6fe567a2a
serii <seriiburduja@gmail.com>
2023-12-07 11:51:26 +0200

    upd: stikcy

--- gsap/gsap-sticky.js
+++ gsap/gsap-sticky.js
@@ -1,19 +1,20 @@
 import ScrollTrigger from "gsap/ScrollTrigger";
 export default function SingleProductSticky() {
-    const productSection = document.querySelectorAll(".single-product__wrap");
-    console.log(productSection, "productSection");
+  gsap.registerPlugin(ScrollTrigger);
+  const productSection = document.querySelectorAll(".single-product__wrap");
+  console.log(productSection, "productSection");
 
-    if (window.innerWidth > 850) {
-        productSection.forEach((section) => {
-            const img = section.querySelector(".single-product__img");
-            ScrollTrigger.create({
-                trigger: section,
-                start: "top +=80",
-                end: "bottom center",
-                pin: img,
-                pinSpacing: false,
-                // markers: true,
-            });
-        });
-    }
+  if (window.innerWidth > 850) {
+    productSection.forEach((section) => {
+      const img = section.querySelector(".single-product__img");
+      ScrollTrigger.create({
+        trigger: section,
+        start: "top +=80",
+        end: "bottom center",
+        pin: img,
+        pinSpacing: false,
+        // markers: true,
+      });
+    });
+  }
 }
