c4b81a9487c1ece0dad2ac8c1e25d52eb895b4e4
serii <seriiburduja@gmail.com>
2023-12-19 13:58:38 +0200

    updated by script at 2023-12-19 13:58:37

--- Javascript/unique-id.js
+++ Javascript/unique-id.js
@@ -1,1 +1,12 @@
-const id = new Date().getTime().toString();
+function generateUniqueId() {
+  // Get the current timestamp
+  const timestamp = new Date().getTime();
+
+  // Generate a random number (you can use a more sophisticated method if needed)
+  const random = Math.floor(Math.random() * 1000);
+
+  // Combine the timestamp and random number to create a unique ID
+  const uniqueId = `${timestamp}-${random}`;
+
+  return uniqueId;
+}
