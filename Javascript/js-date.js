c4d1036cb014ed6fb8707a6cdbf0054baba1f662
serii <seriiburduja@gmail.com>
2024-03-26 13:44:13 +0200

    feat: date

--- Javascript/js-date.js
+++ Javascript/js-date.js
@@ -1,7 +1,8 @@
-export const useFormatDate = (date) => {
+// month/date/year
+export const useFormatDate = (date: string, lang = 'en') => {
     const sections = date.split(' ');
     const date_arr = sections[0].split('/');
-    const months = {
+    const months_it = {
         '01': 'Gennaio',
         '02': 'Febbraio',
         '03': 'Marzo',
@@ -15,6 +16,35 @@ export const useFormatDate = (date) => {
         '11': 'Novembre',
         '12': 'Dicembre',
     };
+    const months_en = {
+        '01': 'January',
+        '02': 'February',
+        '03': 'March',
+        '04': 'April',
+        '05': 'May',
+        '06': 'June',
+        '07': 'July',
+        '08': 'August',
+        '09': 'September',
+        '10': 'October',
+        '11': 'November',
+        '12': 'December',
+    };
+    const months_ro = {
+        '01': 'Ianuarie',
+        '02': 'Februarie',
+        '03': 'Martie',
+        '04': 'Aprilie',
+        '05': 'Mai',
+        '06': 'Iunie',
+        '07': 'Iulie',
+        '08': 'August',
+        '09': 'Septembrie',
+        '10': 'Octombrie',
+        '11': 'Noiembrie',
+        '12': 'Decembrie',
+    }
+    const months = lang === 'it' ? months_it : lang === 'en' ? months_en : months_ro;
     const gsDayNames = [
         'Domenica',
         'Lunedì',
@@ -25,9 +55,9 @@ export const useFormatDate = (date) => {
         'Sabato',
     ];
     const time = sections[1];
-    const date_number = date_arr[0];
-    const month_number = date_arr[1];
-    const month_word = months[date_arr[1]];
+    const date_number = date_arr[1];
+    const month_number = date_arr[0];
+    const month_word = months[date_arr[0]];
     const year = date_arr[2];
     const param_for_date = date_arr[2] + '-' + date_arr[1] + '-' + date_arr[0];
     const date_default = new Date(param_for_date);
@@ -41,40 +71,3 @@ export const useFormatDate = (date) => {
         time
     }
 }
-
-formatDate(date) {
-  let options = {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-    hour12: false
-  };
-
-  return new Intl.DateTimeFormat("ro", options).format(new Date(date));
-}
-
-function fmt(date, format = 'YYYY-MM-DDThh:mm:ss') {
-  const pad2 = (n) => n.toString().padStart(2, '0');
-
-  const map = {
-    YYYY: date.getFullYear(),
-    MM: pad2(date.getMonth() + 1),
-    DD: pad2(date.getDate()),
-    hh: pad2(date.getHours()),
-    mm: pad2(date.getMinutes()),
-    ss: pad2(date.getSeconds()),
-  };
-
-  return Object.entries(map).reduce((prev, entry) => prev.replace(...entry), format);
-}
-
-// Usage
-console.log(
-  fmt(new Date(), 'YYYY-MM-DDThh:mm:ss') // '2023-03-04T10:30:24'
-);
-console.log(
-  fmt(new Date(), 'MM/DD/YYYY, hh:mm:ss') // '03/04/2023, 10:30:24'
-);
