8d7057b19cfa713ecab8f55b81d396157a3edf5b
serii <seriiburduja@gmail.com>
2024-06-18 18:02:50 +0300

    upd: count-numbers

--- Javascript/count-number-on-scroll.js
+++ Javascript/count-number-on-scroll.js
@@ -1,23 +1,44 @@
-  $(window).scroll(startCounter);
-  function startCounter() {
-    if ($(window).scrollTop() > 200) {
-      $(window).off("scroll", startCounter);
-      $('.donation-goes__number').each(function () {
-        const $this = $(this);
-        let text = $this.text();
-        let data_number = $this.data('number');
-        text = text.replace(/,/g, '');
-        console.log(text, "text");
-        jQuery({Counter: 0}).animate({Counter: text}, {
-          duration: 1000,
-          easing: 'swing',
-          step: function () {
-            $this.text(Math.ceil(this.Counter));
-          },
-          complete: function () {
-            $this.text(data_number);
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+export default function animateNumbers() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  let once = false;
+
+  ScrollTrigger.create({
+    trigger: ".years",
+    start: "top 0",
+    // end: "bottom 90%",
+    pinSpacing: false,
+    // markers: true,
+    onEnter: () => {
+      if (!once) {
+        const numbers = document.querySelectorAll(".numbers__title");
+        numbers.forEach((number, index) => {
+          const span = number.querySelector("span");
+          const final_val = parseInt(span?.textContent);
+          span.textContent = "0";
+          if (index === 0) {
+            increment(span, final_val, 40);
+          } else if (index === 1) {
+            increment(span, final_val, 100);
+          } else {
+            increment(span, final_val, 30);
           }
         });
-      });
-    }
+      }
+      once = true;
+    },
+  });
+  function increment(elem, finalVal, timeout) {
+    let currVal = parseInt(elem.innerHTML);
+    let interval = setInterval(() => {
+      if (currVal < finalVal) {
+        currVal++;
+        elem.innerHTML = currVal;
+      } else {
+        clearInterval(interval);
+      }
+    }, timeout);
   }
+}
