eb36d03107cdb01535736e2d7db5d845c77b4b10
serii burduja <seriiburduja@gmail.com>
2024-10-17 08:31:32 +0300

    feat: py-error fonetic

--- Python/py-error.py
+++ Python/py-error.py
@@ -14,6 +14,26 @@ print(l[3])
 print('2' + 2)
 
 
+# =============================
+import pandas
+
+data = pandas.read_csv("nato.csv")
+nato_dict = {row.letter:row.code for (_, row) in data.iterrows()}
+print(f"nato_dict: {nato_dict}")
+
+def generate_phonetic():
+    user_input = input("Enter a word: ").upper()
+    if user_input != '':
+        try:
+            words = [nato_dict[item] for item in user_input]
+        except KeyError as error_message:
+            print("Please enter only letters")
+            generate_phonetic()
+        else:
+            print(f"words: {words}")
+
+generate_phonetic()
+
 try:
     file = open("a_file.txt")
     a_dictionary = {"key": "value"}
@@ -30,3 +50,5 @@ finally:
     # file.close()
     # print("File was closed.")
     raise TypeError("This is an error that I made up.")
+
+
