a59012df52f4650a4059c65991f2a34adf51a671
serii burduja <seriiburduja@gmail.com>
2024-10-26 21:48:54 +0300

    feat: php variable

---
+++ Php/php-variable-in-string.php
@@ -0,0 +1,8 @@
+<?php
+
+$price = 100;
+$color = 'red';
+
+// $price in double quotes
+// $color in curly braces if its a complex variable like $color['red']
+$message = "The price of the shirt is $price and the color is {$color}";
