c430dbb4d4a08254d41d26d88a0317aa8644b0da
serii burduja <seriiburduja@gmail.com>
2024-12-12 16:18:28 +0200

    feat: vue transitions

---
+++ Vue/vue-transition.vue
@@ -0,0 +1,31 @@
+<template>
+  <Transition name="property">
+    <div v-if="show">hello</div>
+  </Transition>
+</template>
+<style lang="scss">
+// before enter
+.property-enter-from {
+  opacity: 0;
+}
+// after enter
+.property-enter-to {
+  opacity: 1;
+}
+// while enter
+.property-enter-active {
+  transition: opacity 0.5s;
+}
+// before leave
+.property-leave-from {
+  opacity: 1;
+}
+// after leave
+.property-leave-to {
+  opacity: 0;
+}
+// while leave
+.property-leave-active {
+  transition: opacity 0.3s;
+}
+</style>
