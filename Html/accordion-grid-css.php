e6fc4c61fb02f6660df9a03cb7c5fd6d7a11c204
serii burduja <seriiburduja@gmail.com>
2025-03-03 11:15:32 +0200

    feat: accordion grid css

---
+++ Html/accordion-grid-css.php
@@ -0,0 +1,43 @@
+<div class="services__item">
+  <div class="services__img">
+    <img src="<?php echo $image['url']; ?>" class="services__image" alt="<?php echo $image['alt']; ?>" />
+  </div>
+  <div class="services__content">
+    <h3 class="services__subtitle"><?php echo $subtitle; ?></h3>
+    <div class="services__text text">
+      <div class="wrapper">
+        <div class="inner">
+          <?php echo $text; ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<style>
+  .wrapper {
+    display: grid;
+    grid-template-rows: 0fr;
+    transition: grid-template-rows 0.5s ease-out;
+  }
+
+  .wrapper.is-open {
+    grid-template-rows: 1fr;
+  }
+
+  .inner {
+    overflow: hidden;
+  }
+</style>
+<script>
+  export default function servicesText() {
+    const items = document.querySelectorAll(".services__item");
+    items.forEach((item) => {
+      item.addEventListener("mouseenter", (e) => {
+        const wrapper = item.querySelector(".wrapper");
+        if (!wrapper) return;
+        if (wrapper.classList.contains("is-open")) return;
+        wrapper.classList.add("is-open");
+      });
+    });
+  }
+</script>
