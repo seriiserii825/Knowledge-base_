6bb683c9af1dd70a518315e35965c01b00d518a1
serii burduja <seriiburduja@gmail.com>
2025-03-07 10:05:05 +0200

    feat: ts-array

---
+++ Typescript/ts-array.ts
@@ -0,0 +1,4 @@
+const first: string[] = ['a', 'b', 'c'];
+const second: Array<string> = ['a', 'b', 'c'];
+const third: (string | number)[] = ['a', 'b', 'c', 1];
+const fourth: Array<string | number> = ['a', 'b', 'c', 1];
