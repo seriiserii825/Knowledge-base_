3e0c843b22263c8af0cbd38662d5f389a604097d
serii burduja <seriiburduja@gmail.com>
2025-03-07 13:09:23 +0200

    upd: never asert

--- Typescript/ts-never.ts
+++ Typescript/ts-never.ts
@@ -11,3 +11,26 @@ function getUsername(): string | never {
 }
 
 const user_name = getUsername();
+
+///
+type TCurrency = "USD" | "EUR" | "RUB" | "YEN";
+
+// assert enxhaustive
+function getCurrency(currency: TCurrency): string | never {
+  if (currency === "USD") {
+    return "$";
+  }
+  if (currency === "EUR") {
+    return "€";
+  }
+
+  if (currency === "RUB") {
+    return "₽";
+  }
+
+  const _: never = currency; // have new currency but not implemented(YEN)
+
+  throw new Error("Currency not found");
+}
+
+getCurrency('YEN');
