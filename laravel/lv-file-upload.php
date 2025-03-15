420055c7fb4ccd348f8b06dd177f9f2ac0be882f
serii burduja <seriiburduja@gmail.com>
2025-03-15 09:42:16 +0200

    upd: upload file lv

--- laravel/lv-file-upload.php
+++ laravel/lv-file-upload.php
@@ -6,11 +6,26 @@ use Illuminate\Http\UploadedFile;
 
 trait FileUpload
 {
+    // with unique file name
     public function uploadFile(UploadedFile $file, string $directory = 'uploads'): string
     {
         $filename = 'educode_'. uniqid() . '_' . $file->getClientOriginalExtension();
         $file->move(public_path($directory), $filename);
         return '/' . $directory . '/' . $filename;
+    }
+
+    // with original file name
+    public function uploadFile(UploadedFile $file, string $directory = 'uploads'): string
+    {
+        $file_name = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $full_file_name = $file_name .'.'. $file->getClientOriginalExtension();
+        // check if file already exists in public/uploads directory
+        if (file_exists(public_path($directory . '/' . $full_file_name))) {
+            $full_file_name = $file_name . '_copy.' . $file->getClientOriginalExtension();
+        }
+
+        $file->move(public_path($directory), $full_file_name);
+        return '/' . $directory . '/' . $full_file_name;
     }
 }
 
