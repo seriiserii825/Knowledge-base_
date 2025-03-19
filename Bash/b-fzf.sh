7d39136e12e11bc2e0759d78bbae871e49729c45
serii burduja <seriiburduja@gmail.com>
2025-03-19 11:22:43 +0200

    feat: fzf find dir

--- Bash/b-fzf.sh
+++ Bash/b-fzf.sh
@@ -4,3 +4,6 @@ file1_path=$( fzf )
 file2_path=$( ls | grep -v $file1_path | fzf )
 
 echo "file1_path: $file1_path"
+
+## find directories
+dir_path=$(find . -type d 2>/dev/null | fzf --height 40% --reverse)
