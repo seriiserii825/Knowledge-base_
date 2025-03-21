b3543ad78ebf99dc6618014d02d5b22804010c4f
serii burduja <seriiburduja@gmail.com>
2025-03-21 23:35:30 +0200

    feat: checkbox

--- laravel/lv-checkbox.php
+++ laravel/lv-checkbox.php
@@ -7,3 +7,17 @@
         </label>
     </div>
 @endforeach
+
+
+<div class="add_course_more_info_checkbox">
+    <div class="form-check">
+        <input name="qna" value="1" class="form-check-input" type="checkbox"
+            id="flexCheckDefault" {{ old('qna', $course->qna) ? 'checked' : '' }}>
+        <label class="form-check-label" for="flexCheckDefault">Q&A</label>
+    </div>
+    <div class="form-check">
+        <input name="certificate" value="1" class="form-check-input" type="checkbox"
+            id="flexCheckDefault2" {{ old('certificate', $course->certificate) ? 'checked' : '' }}>
+        <label class="form-check-label" for="flexCheckDefault2">Completion Certificate</label>
+    </div>
+</div>
