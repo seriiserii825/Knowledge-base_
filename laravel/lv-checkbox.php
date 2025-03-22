027559ff2d07e8d65060c6967b6f0c6feba27a6b
serii burduja <seriiburduja@gmail.com>
2025-03-22 06:50:38 +0200

    upd: lv checked

--- laravel/lv-checkbox.php
+++ laravel/lv-checkbox.php
@@ -1,11 +1,14 @@
+<input name="qna" value="1" class="form-check-input" type="checkbox"
+    id="flexCheckDefault" @checked($course->qna == 1)>
+
 @foreach ($languages as $language)
-    <div class="form-check">
-        <input class="form-check-input" type="radio" value="{{ $language->id }}"
-            {{ old('course_language_id', $current_language_id ?? '') == $language->id ? 'checked' : '' }}>
-        <label class="form-check-label" for="js-language-{{ $language->id }}">
-            {{ $language->name }}
-        </label>
-    </div>
+<div class="form-check">
+    <input class="form-check-input" type="radio" value="{{ $language->id }}"
+        {{ old('course_language_id', $current_language_id ?? '') == $language->id ? 'checked' : '' }}>
+    <label class="form-check-label" for="js-language-{{ $language->id }}">
+        {{ $language->name }}
+    </label>
+</div>
 @endforeach
 
 
