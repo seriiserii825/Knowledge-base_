ac5ad5c5f5af88a99c94d48b5bd247d854960dd1
serii burduja <seriiburduja@gmail.com>
2025-03-24 10:14:50 +0200

    feat: select component

--- laravel/lv-select.php
+++ laravel/lv-select.php
@@ -12,3 +12,20 @@
     @endif
     @endforeach
 </select>
+
+
+
+<!-- select component -->
+@props(['name', 'value' => '', 'label' => '', 'id' => '', 'options' => []])
+@if ($label)
+    <label class="form-label">{{ $label }}</label>
+@endif
+<select class="form-select form-control" id="{{ $id }}" name="{{ $name }}">
+    @foreach ($options as $option_value => $label)
+        <option value="{{ $option_value }}"
+            {{ old($name, $value ?? '') == $option_value ? 'selected' : '' }}
+            >
+            {{ $label }}
+        </option>
+    @endforeach
+</select>
