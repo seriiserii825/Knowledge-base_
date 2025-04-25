f407c20876e8a4f1eab3671a5281efc62b434ecd
serii burduja <seriiburduja@gmail.com>
2025-04-25 15:47:32 +0300

    feat: d-date

--- Bash/b-func.sh
+++ Bash/b-func.sh
@@ -1,4 +1,18 @@
+## variable scope
+global_var="global_var"
 
+function test {
+  local local_var="local_var"
+  echo "Inside function: $local_var"
+  echo "Inside function: $global_var"
+}
+
+echo "Before function: $global_var"
+test
+echo "After function: $local_var" # This will cause an error because local_var is not accessible outside the function
+
+
+## function with arguments
 function parseArray {
   result=()
   array=("$@")
