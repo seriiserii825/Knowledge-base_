e2d752cd2dfaf3b78dba466d2d5b2127d16797f2
serii burduja <seriiburduja@gmail.com>
2025-05-03 12:28:55 +0300

    loop throw file

--- Bash/b-loop.sh
+++ Bash/b-loop.sh
@@ -1,6 +1,28 @@
 ### while
 
 ```
+  repos=("${(@f)$(< "$file_path")}")
+
+  for line in "${repos[@]}"; do
+    [[ -z "$line" ]] && continue
+
+    if [[ ! -d "$line/.git" ]]; then
+      echo "Not a Git repository: $line"
+      continue
+    fi
+
+    echo "Processing repository: $line"
+
+    cd "$line" || continue
+
+    if [[ -n $(git status --porcelain) ]]; then
+      echo "Uncommitted changes in $line:"
+      gitPush $script_dir
+    fi
+  done
+```
+
+```
 colors="red green blue"
 for color in $colors
 do
