b06d59d79626f9a3b08a54d504801e6b4f03e5ab
serii burduja <seriiburduja@gmail.com>
2025-05-05 08:40:51 +0300

    awk and cut

---
+++ Bash/b-awk.sh
@@ -0,0 +1,3 @@
+## example
+serii:x:1000:1000::/home/serii:/usr/bin/zsh
+shor_name=$(cat /etc/passwd | grep $name | awk -F : '{print $1}')
---
+++ Bash/b-cut.sh
@@ -0,0 +1,4 @@
+serii:x:1000:1000::/home/serii:/usr/bin/zsh
+shor_name=$(cat /etc/passwd | grep $name | awk -F : '{print $6}')
+/home/serii
+shor_name=$(cat /etc/passwd | grep $name | awk -F : '{print $6}' | cut -d / -f 3)
