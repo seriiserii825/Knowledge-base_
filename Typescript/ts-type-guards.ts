c675ae5b80bad7ec1a9368a22004f132951ee28f
serii <seriiburduja@gmail.com>
2025-09-20 10:14:05 +0300

    feat: update type guard

--- Typescript/ts-type-guards.ts
+++ Typescript/ts-type-guards.ts
@@ -1,22 +1,21 @@
-type TAdminUser = {
-  type: "admin";
+type TUser = {
   id: number;
   name: string;
+};
+
+type TAdminUser = TUser & {
+  type: "admin";
   accessLevel: number;
 };
 
-type TManagerUser = {
+type TManagerUser = TUser & {
   type: "manager";
-  id: number;
-  name: string;
   accessLevel: number;
   roles: string[];
 };
 
-type TClientUser = {
+type TClientUser = TUser & {
   type: "client";
-  id: number;
-  name: string;
 };
 
 function loadUser(): TAdminUser | TManagerUser | TClientUser {
@@ -33,4 +32,53 @@ const user = loadUser();
 
 if (user.type === "manager") {
   console.log(`Manager Roles: ${user.roles.join(", ")}`);
+}
+
+///////////////////////////////////////////////////////
+
+type Component = () => string;
+
+type RouteRecordBase = {
+  path: string;
+};
+
+type RouteRecordComponent = RouteRecordBase & {
+  type: "component";
+  component: Component;
+  children?: RouteRecord[];
+};
+type RouteRecordRedirect = RouteRecordBase & {
+  type: "redirect";
+  redirect: string;
+};
+
+type RouteRecord = RouteRecordRedirect | RouteRecordComponent;
+
+createRouter([
+  {
+    type: "component",
+    path: "/",
+    component: () => "Home Page",
+  },
+  {
+    type: "redirect",
+    path: "/about",
+    redirect: "/info",
+  },
+  {
+    type: "component",
+    path: "/products",
+    component: () => "Products Page",
+    children: [
+      {
+        type: "component",
+        path: "electronics",
+        component: () => "Electronics Page",
+      },
+    ],
+  },
+]);
+
+function createRouter(routes: RouteRecord[]) {
+  console.log("Router initialized with routes:", routes);
 }
