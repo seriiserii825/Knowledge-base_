ef054da73cbe28d8d20b49645ec9c79ae315b90e
serii <seriiburduja@gmail.com>
2025-09-20 10:48:34 +0300

    feat: ts update

--- Typescript/ts-interface.ts
+++
@@ -1,6 +0,0 @@
-export interface I_SelectOptions {
-    [index: number]: { id: number; title: string; };
-}
-
-// empty object
-let lc_storage = {} as ILcStorage;
--- Typescript/ts-literal.ts
+++
@@ -1,9 +0,0 @@
-function getStatus(status: 'success' | 'error') {
-  if (status === 'success') {
-    return 'success';
-  } else {
-    return 'error';
-  }
-}
-
-const my_status = getStatus('error');
