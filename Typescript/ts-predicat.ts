3c749a877b71d1468e5b71b8b5db5faf21f7ee8b
serii <seriiburduja@gmail.com>
2025-09-27 11:16:06 +0300

    feat: update predicat

--- Typescript/ts-predicat.ts
+++ Typescript/ts-predicat.ts
@@ -8,9 +8,18 @@ function isManager(user: TUser): user is TManagerUser {
   return user.type === "manager";
 }
 
+const users = loadUser();
 
 // Example of usage with filter and map
 users.filter((user) => user.type === "manager").map((user) => user.roles);
 
 // Using the type predicate function
 users.filter(isManager).map((user) => user.roles);
+
+function userFilterOriginal<T>(type: T) {
+  return (u: TUser) => u.type === type;
+}
+
+function userFilter<T extends TUser["type"]>(type_prop: T) {
+  return (u: TUser): u is Extract<TUser, { type: typeof type_prop }> => u.type === type_prop;
+}
