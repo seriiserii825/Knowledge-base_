8a18fb34ca4cc8bcae2ae9f99d97174165c56c58
serii <seriiburduja@gmail.com>
2025-10-09 08:31:39 +0300

    feat: assert

--- Typescript/ts-type-assertion-not-null.ts
+++ Typescript/ts-type-assertion-not-null.ts
@@ -1,16 +1,32 @@
-// Non-null assertion operator
-const app = document.getElementById("app")!;
-app.innerHTML = "Hello, TypeScript!";
-
-// Type assertion
-const a = document.querySelector("a") as HTMLAnchorElement;
-console.log(a.href);
-
-// Alternative type assertion syntax
-const b = <HTMLAnchorElement>document.querySelector("a");
-console.log(b.href);
-
-const input = document.querySelector("input");
-if (input instanceof HTMLInputElement) {
-  input.value = "TypeScript is awesome!";
+type Person = {
+  name: string;
+  age: number;
+  profession: string;
+};
+function createDemoPerson(name: string) {
+  const person = {} as Person;
+  person.name = name;
+  person.age = Math.floor(Math.random() * 95);
+  // person.profession is not set and no error is thrown due to type assertion
+  return person;
 }
+
+const person = createDemoPerson("Alice");
+console.log(person.profession, "person.profession");
+
+type Person2 = {
+  name: string;
+  age: number;
+  profession: string;
+};
+function createDemoPerson2(name: string) {
+  // catch the error by initializing all properties
+  const person: Person2 = {
+    name,
+    age: Math.floor(Math.random() * 95),
+  };
+  return person;
+}
+
+const person2 = createDemoPerson2("Bob");
+console.log(person2.profession, "person2.profession");
