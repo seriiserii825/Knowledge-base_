3ff4c039703139a161c4be8f950721b3993cc341
serii burduja <seriiburduja@gmail.com>
2025-10-31 12:43:14 +0200

    feat: fzf

--- Bash/b-fzf.sh
+++ Bash/b-fzf.sh
@@ -5,5 +5,8 @@ file2_path=$( ls | grep -v $file1_path | fzf )
 
 echo "file1_path: $file1_path"
 
+# find jpg files
+file_path=$(find . -maxdepth 1 -type f -iname "*.jpg" -printf "%f\n" | fzf --prompt="Select JPG: " --height=80% --reverse)
+
 ## find directories
 dir_path=$(find . -type d 2>/dev/null | fzf --height 40% --reverse)
