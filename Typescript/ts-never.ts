3938a50ab66770dd84635febef84b0a4ab702175
serii <seriiburduja@gmail.com>
2025-11-07 07:36:21 +0200

    feat: never update

--- Typescript/ts-never.ts
+++ Typescript/ts-never.ts
@@ -12,25 +12,56 @@ function getUsername(): string | never {
 
 const user_name = getUsername();
 
-///
-type TCurrency = "USD" | "EUR" | "RUB" | "YEN";
+export default function neverCase() {
+  type TCar = {
+    name: 'car';
+    engine: string;
+    wheels: number;
+  };
+  type TShip = {
+    name: 'ship';
+    engine: string;
+    sail: number;
+  };
 
-// assert enxhaustive
-function getCurrency(currency: TCurrency): string | never {
-  if (currency === "USD") {
-    return "$";
+  type TPlane = {
+    name: 'plane';
+    engine: string;
+    wings: number;
+  };
+
+  type TBus = {
+    name: 'bus';
+    engine: string;
+    seats: number;
+  };
+
+  type TVehicle = TCar | TShip | TPlane | TBus; 
+
+  function isCar(car: TVehicle): car is TCar {
+    return "wheels" in car;
   }
-  if (currency === "EUR") {
-    return "€";
+  function isShip(ship: TVehicle): ship is TShip {
+    return "sail" in ship;
   }
 
-  if (currency === "RUB") {
-    return "₽";
+  function repairVehicle(vehicle: TVehicle): void {
+    switch (vehicle.name) {
+      case 'car':
+        console.log(`Repairing car with ${vehicle.wheels} wheels and engine ${vehicle.engine}`);
+        break;
+      case 'ship':
+        console.log(`Repairing ship with ${vehicle.sail} sails and engine ${vehicle.engine}`);
+        break;
+      case 'plane':
+        console.log(`Repairing plane with ${vehicle.wings} wings and engine ${vehicle.engine}`);
+        break;
+      case 'bus':
+        console.log(`Repairing bus with ${vehicle.seats} seats and engine ${vehicle.engine}`);
+        break;
+      default:
+        const _exhaustiveCheck: never = vehicle;
+        throw new Error(`Unhandled vehicle type: ${_exhaustiveCheck}`);
+    }
   }
-
-  const _: never = currency; // have new currency but not implemented(YEN)
-
-  throw new Error("Currency not found");
 }
-
-getCurrency('YEN');
