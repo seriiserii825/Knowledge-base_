54193dbb32befe57993261d623da062f21ed14eb
serii burduja <seriiburduja@gmail.com>
2025-11-12 13:15:28 +0200

    feat: access index

--- Typescript/ts-acces-by-index.ts
+++ Typescript/ts-acces-by-index.ts
@@ -18,12 +18,16 @@ export default function accessIndexType() {
 
   // Using Access Index Type to create a new type based on an existing type
   type TDebts = TCompany["debts"];
+  // Creating a type that represents the debts property of TCompany using a variable
   type TDebtsType = TCompany[typeof debts];
 
+  // Creating a type that represents the owner property inside the management object
   type TCOmpanyOwnerTGype = TCompany["management"]["owner"];
 
+  // Creating a type that represents all possible keys of the departments object
   type TCmpanyDepartment = keyof TCompany["departments"];
 
+  // Creating a type that represents all possible value types of TCompany
   type TCompanyKeysType = TCompany[keyof TCompany]
 
   const company: TCompany = {
@@ -40,4 +44,14 @@ export default function accessIndexType() {
       owner: "Alice Johnson"
     }
   };
+
+  function printDebs<T, K extends keyof T, S extends keyof T>(company: T, name: K, debts: S) {
+    const message = `Company: ${company[name]} has debts ${company[debts]}`;
+    console.log(message);
+  }
+  printDebs(company, "name", "debts");
+
+  type TCompanyTypeKeys = keyof TCompany;
+
+  type TCompanyObjectKeys = keyof typeof company;
 }
