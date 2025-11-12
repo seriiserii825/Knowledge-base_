4a52aa33d6978c9e26064e3757110b2238068d29
serii burduja <seriiburduja@gmail.com>
2025-11-12 18:03:00 +0200

    feat: mapped types

--- Typescript/ts-mapped-types.ts
+++ Typescript/ts-mapped-types.ts
@@ -16,3 +16,41 @@ type TopNavStateMappedType = {
 };
 
 type TopNavStatePick = Pick<State, 'userId' | 'pageTitle' | 'recentFiles'>;
+
+
+export default function mappedTypes() {
+  type Keys = "name" | "age" | "location";
+  type User = {
+    [K in Keys]: string;
+  };
+
+  const alex: User = {
+    name: "Alex",
+    age: "30",
+    location: "New York",
+  };
+
+  type Languages = {
+    ru: "ru";
+    en: "en";
+    de: "de";
+  };
+
+  type MyLanguages = {
+    [K in keyof Languages]: string;
+  };
+
+  const original_langs: Languages = {
+    ru: "ru",
+    en: "en",
+    de: "de",
+  };
+
+  const myLangs: MyLanguages = {
+    ru: "Russian",
+    en: "English",
+    de: "German",
+  };
+  console.log(original_langs, "original_langs");
+  console.log(myLangs, "myLangs");
+}
