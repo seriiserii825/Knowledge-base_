0a65d378350b06de52f263b274e9630cd73a0216
serii burduja <seriiburduja@gmail.com>
2025-12-24 11:06:44 +0200

    feat: debounce

--- Javascript/debounce.js
+++ Javascript/debounce.js
@@ -15,3 +15,21 @@ function onChange(e) {
 
 onChange = debounce(onChange, 300);
 document.getElementById("search").addEventListener("keyup", onChange);
+
+// for vue
+import { ref, watch } from "vue";
+import { debounce } from "lodash";
+
+const search = ref("");
+const first_load = ref(true);
+
+// Create debounced function
+const debouncedFetchProjects = debounce(async () => {
+  await fetchProjects();
+}, 300);
+
+watch(search, () => {
+  if (first_load.value) {
+    debouncedFetchProjects();
+  }
+});
