8b88000f50027119cde7056b88298f8a9baf83d2
serii burduja <seriiburduja@gmail.com>
2025-12-24 11:00:24 +0200

    feat: wp search

--- Wordrpess/wp-search.php
+++ Wordrpess/wp-search.php
@@ -6,16 +6,27 @@ $args = array(
     'order' => $_POST['order'],
     'search_prod_title' => $_POST['search'],
 );
-add_filter( 'posts_where', 'title_filter', 10, 2 );
 $query = new WP_Query($args);
-remove_filter( 'posts_where', 'title_filter', 10, 2 );
 
 // functions.php
-function title_filter( $where, &$wp_query ){
-    global $wpdb;
-    if ( $search_term = $wp_query->get( 'search_prod_title' ) ) {
-        $where .= ' AND ' . $wpdb->posts . '.post_title LIKE \'%' . esc_sql( $wpdb->esc_like( $search_term ) ) . '%\'';
-    }
+
+add_filter( 'posts_where', 'title_filter', 10, 2 );
+function title_filter($where, $wp_query)
+{
+  global $wpdb;
+  
+  // Check if this is a query for 'progetti' post type
+  $post_type = $wp_query->get('post_type');
+  
+  // Only apply to 'progetti' post type
+  if ($post_type !== 'progetti') {
     return $where;
+  }
+  
+  if ($search_term = $wp_query->get('search_prod_title')) {
+    $where .= ' AND ' . $wpdb->posts . '.post_title LIKE \'%' . esc_sql($wpdb->esc_like($search_term)) . '%\'';
+  }
+  
+  return $where;
 }
-  ?>
+add_filter('posts_where', 'title_filter', 10, 2);
