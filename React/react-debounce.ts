fa1d0ad18531444a7fdaf071aa357ffa7c112feb
serii burduja <seriiburduja@gmail.com>
2026-03-02 16:23:01 +0200

    feat: zustand and usedebounce

---
+++ React/react-debounce.ts
@@ -0,0 +1,28 @@
+// hooks/useDebounce.ts
+import { useEffect, useState } from "react";
+
+export function useDebounce<T>(value: T, delay = 500): T {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    // Очистка — очень важно!
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
+// usage
+
+const [search, setSearch] = useState("");
+
+const debouncedSearch = useDebounce(search, 500);
+
+useEffect(() => {
+  // Здесь можно выполнять запрос к API с использованием debouncedSearch
+  console.log("Debounced Search:", debouncedSearch);
+}, [debouncedSearch]);
