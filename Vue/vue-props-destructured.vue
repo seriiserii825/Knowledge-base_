073d598591d98c5cab6f884bd9e7653218e207d4
seriiserii825 <seriiburduja@gmail.com>
2026-05-29 08:08:27 +0300

    feat: props destructure

---
+++ Vue/vue-props-destructured.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  interface Props {
+    title?: string;
+    count?: number;
+    enabled?: boolean;
+  }
+
+  const { title = "Default title", count = 0, enabled = true } = defineProps<Props>();
+</script>
