bc944174d062376be87874f6237449b48ee68a46
serii burduja <seriiburduja@gmail.com>
2026-06-10 12:51:01 +0300

    feat: vue html

--- Vue/vue-v-html.vue
+++ Vue/vue-v-html.vue
@@ -4,6 +4,15 @@
   const safeHtml = DOMPurify.sanitize(apiContent);
 </script>
 
+<script>
+  import DOMPurify from 'dompurify';
+
+  export default function useSafeDisplay(value: string) {
+    const sanitizedValue = DOMPurify.sanitize(value);
+    return sanitizedValue;
+  }
+</script>
+
 <template>
   <div v-html="safeHtml"></div>
 </template>
