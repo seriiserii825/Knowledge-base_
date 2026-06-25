172431c0eb3db503fc4388bf55d257d75394fa23
serii burduja <seriiburduja@gmail.com>
2026-06-25 16:14:16 +0300

    feat: click outside change

--- Hooks/useDetectOutsideClick.ts
+++ Hooks/useDetectOutsideClick.ts
@@ -1,16 +1,21 @@
-import { onBeforeUnmount, onMounted, type Ref } from "vue";
+import { type Ref, onBeforeUnmount, onMounted } from "vue";
 
 export default function useDetectOutsideClick(
   component: Ref<HTMLElement | null>,
+  clicked_component: Ref<HTMLElement | null>,
   callback: () => void,
 ) {
-  if (!component) return;
+  if (!component || !clicked_component) return;
 
   const listener = (event: MouseEvent) => {
     const target = event.target as Node | null;
     const element = component.value;
 
     if (!element || !target) return;
+    const is_clicked_component = clicked_component.value?.contains(target);
+    if (is_clicked_component) {
+      return;
+    }
 
     // click inside component → do nothing
     if (target === element || event.composedPath().includes(element)) {
@@ -42,12 +47,15 @@ import useDetectOutsideClick from '@/composables/useDetectOutsideClick';
 const isOpen = ref(true);
 const dropdown = ref<HTMLElement | null>(null);
 
-useDetectOutsideClick(dropdown, () => {
+useDetectOutsideClick(dropdown, toggle_btn, () => {
   isOpen.value = false;
 });
 </script>
 
 <template>
+    <button ref="toggle_btn" class="share-estate__icon" @click="toggleOpen">
+      <IconShare />
+    </button>
   <div
     v-if="isOpen"
     ref="dropdown"
